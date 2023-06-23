bdfbc8d8e7fea72f7416da292e62ea5e02f0982f
carlomagg <you@example.com>
2023-06-23 15:59:18 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, jsonify
+import pandas as pd
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'listings.db'
+CSV_FILE = 'data.csv'
+
+def load_data():
+    data = pd.read_csv(CSV_FILE)
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    # Drop the existing table if it exists
+    cursor.execute('DROP TABLE IF EXISTS listings')
+
+    # Create the listings table
+    cursor.execute('''CREATE TABLE IF NOT EXISTS listings (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        homeDetailUrl TEXT,
+                        price_to_rent_ratio REAL,
+                        imgSrc TEXT,
+                        country TEXT,
+                        currency TEXT,
+                        homeStatus TEXT,
+                        price INTEGER,
+                        state TEXT,
+                        bedrooms INTEGER,
+                        bathrooms INTEGER,
+                        zipcode TEXT,
+                        city TEXT,
+                        streetName TEXT
+                        )''')
+
+    # Insert the data into the listings table
+    data.to_sql('listings', conn, if_exists='append', index=False)
+
+    conn.commit()
+    conn.close()
+
+@app.route('/listing/<int:listing_id>', methods=['GET'])
+def get_listing(listing_id):
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute(f'SELECT * FROM listings WHERE id = ?', (listing_id,))
+    row = cursor.fetchone()
+
+    if row:
+        listing = {
+            'id': row['id'],
+            'homeDetailUrl': row['homeDetailUrl'],
+            'price_to_rent_ratio': row['price_to_rent_ratio'],
+            'imgSrc': row['imgSrc'],
+            'country': row['country'],
+            'currency': row['currency'],
+            'homeStatus': row['homeStatus'],
+            'price': row['price'],
+            'state': row['state'],
+            'bedrooms': row['bedrooms'],
+            'bathrooms': row['bathrooms'],
+            'zipcode': row['zipcode'],
+            'city': row['city'],
+            'streetName': row['streetName']
+        }
+        return jsonify(listing)
+    else:
+        return jsonify({'message': 'Listing not found'})
+
+@app.route('/listings', methods=['GET'])
+def get_all_listings():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM listings')
+    rows = cursor.fetchall()
+
+    listings = []
+    for row in rows:
+        listing = {
+            'id': row['id'],
+            'homeDetailUrl': row['homeDetailUrl'],
+            'price_to_rent_ratio': row['price_to_rent_ratio'],
+            'imgSrc': row['imgSrc'],
+            'country': row['country'],
+            'currency': row['currency'],
+            'homeStatus': row['homeStatus'],
+            'price': row['price'],
+            'state': row['state'],
+            'bedrooms': row['bedrooms'],
+            'bathrooms': row['bathrooms'],
+            'zipcode': row['zipcode'],
+            'city': row['city'],
+            'streetName': row['streetName']
+        }
+        listings.append(listing)
+
+    return jsonify(listings)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'message': 'Welcome to the Real Estate Listing API'})
+
+if __name__ == '__main__':
+    load_data()
+    app.run(debug=True)
